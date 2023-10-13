86995073d89e7123fa9ccea027542d392b638183
Miguel Alejandro Martín <alu0101209777@ull.edu.es>
2023-10-13 03:52:27 +0100

    min_words_count param added, test_filter_by_word_count_7_and_order_by_comments failing

--- stackcrawler/EntryFilter.py
+++ stackcrawler/EntryFilter.py
@@ -5,8 +5,11 @@ from .Entry import Entry
 class EntryFilter:
     def __init__(self):
         pass
-    
-    def filter_by_word_count_and_order_by_comments(self, entries: List[Entry]) -> List[Entry]:
+
+    def filter_by_word_count_and_order_by_comments(self, entries: List[Entry], min_words_count: int = 5) -> List[Entry]:
+        """
+          Filters entries whose length is greater than 5 and sorts them by comments count
+        """
         return sorted(
             filter(lambda entry: len(entry.title.split()) > 5, entries),
             key=lambda entry: entry.comments_count
--- tests/test_StackCrawler.py
+++ tests/test_StackCrawler.py
@@ -2,31 +2,44 @@ from unittest import TestCase
 from stackcrawler.EntryFilter import EntryFilter
 from stackcrawler.Entry import Entry
 
+
 class TestStackCrawler(TestCase):
 
-    # Should be able to filter all previous entries with more 
+    # Should be able to filter all previous entries with more
     # than five words in the title ordered by the number of comments first.
 
     def get_entries(self):
-      return [
-            Entry(title="Scrollbars Are Becoming a Problem", order_number=1, comments_count=67, points=110),
-            Entry(title="First word discovered in unopened Herculaneum scroll by CS student", order_number=2, comments_count=108, points=458),
-            Entry(title="Desmos 3D graphing calculator", order_number=3, comments_count=70, points=302),
-            Entry(title="REI is Laying Off 275 Employees", order_number=4, comments_count=54, points=27),
+        return [
+            Entry(title="Scrollbars Are Becoming a Problem",
+                  order_number=1, comments_count=67, points=110),
+            Entry(title="First word discovered in unopened Herculaneum scroll by CS student",
+                  order_number=2, comments_count=108, points=458),
+            Entry(title="Desmos 3D graphing calculator",
+                  order_number=3, comments_count=70, points=302),
+            Entry(title="REI is Laying Off 275 Employees",
+                  order_number=4, comments_count=54, points=27),
         ]
 
-
-    def test_filter_by_word_count_and_order_by_comments(self):
-        entry_filter = EntryFilter()    
+    def test_filter_by_word_count_5_and_order_by_comments(self):
+        entry_filter = EntryFilter()
         entries = self.get_entries()
         expected = [
-          Entry(title="REI is Laying Off 275 Employees", order_number=4, comments_count=54, points=27),          
-          Entry(title="First word discovered in unopened Herculaneum scroll by CS student", order_number=2, comments_count=108, points=458),
+            Entry(title="REI is Laying Off 275 Employees",
+                  order_number=4, comments_count=54, points=27),
+            Entry(title="First word discovered in unopened Herculaneum scroll by CS student",
+                  order_number=2, comments_count=108, points=458),
         ]
-        
+
         filtered_entries = entry_filter.filter_by_word_count_and_order_by_comments(entries)
         self.assertEqual(expected, filtered_entries)
 
-    
+    def test_filter_by_word_count_7_and_order_by_comments(self):
+        entry_filter = EntryFilter()
+        entries = self.get_entries()
+        expected = [
+            Entry(title="First word discovered in unopened Herculaneum scroll by CS student",
+                  order_number=2, comments_count=108, points=458),
+        ]
 
-
+        filtered_entries = entry_filter.filter_by_word_count_and_order_by_comments(entries)
+        self.assertEqual(expected, filtered_entries)
