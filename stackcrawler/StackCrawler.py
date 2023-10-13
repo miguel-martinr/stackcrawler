d1f3ba7b29fd29b7be326b5348ffd8863124fb44
Miguel Alejandro Martín <alu0101209777@ull.edu.es>
2023-10-13 12:29:35 +0100

    pycache removed

--- stackcrawler/StackCrawler.py
+++ stackcrawler/StackCrawler.py
@@ -1,14 +1,7 @@
-from typing import List
-from .Entry import Entry
+
 
 class StackCrawler:
-  def __init__(self):
-    pass
-
-  def fetch_entries(self, url: str, limit: int = 30) -> List[Entry]:
-    pass
-
-
+    def __init__(self):
+        pass
 
     
-
