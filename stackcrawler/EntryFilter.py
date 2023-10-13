fda9711ef18e21050fbd94d942035c13dc0a6327
Miguel Alejandro Martín <alu0101209777@ull.edu.es>
2023-10-13 13:48:14 +0100

    comment fix

--- stackcrawler/EntryFilter.py
+++ stackcrawler/EntryFilter.py
@@ -19,7 +19,7 @@ class EntryFilter:
 
     def filter_by_word_count_and_order_by_comments(self, entries: List[Entry], words_count_gt: int = 5) -> List[Entry]:
         """
-          Filters entries whose length is greater than 5 and sorts them by comments count
+          Filters entries whose length is greater than `words_count_gt` and sorts them by comments count
 
           - `words_count_gt`: Words count greather than... Default: 5
         """
@@ -30,7 +30,7 @@ class EntryFilter:
 
     def filter_by_word_count_and_order_by_points(self, entries: List[Entry], words_count_le: int = 5) -> List[Entry]:
         """
-          Filters entries whose length is less than or equal to 5 and sorts them by points
+          Filters entries whose length is less than or equal to `words_count_le` and sorts them by points
 
           - `words_count_le`: Words count less than or equal to... Default: 5
         """
