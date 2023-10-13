9b3aca43a3f048f6bebd4800ac8de57f56bd62ed
Miguel Alejandro Martín <alu0101209777@ull.edu.es>
2023-10-13 13:20:29 +0100

    parse_entries_from_HTML failing

---
+++ stackcrawler/HackerNewsParser.py
@@ -0,0 +1,7 @@
+from .Parser import Parser
+
+class HackerNewsParser(Parser):
+  
+
+  def parse_entries_from_HTML(html_content: str):
+    pass
---
+++ tests/test_HackerNewsParser.py
@@ -0,0 +1,29 @@
+from unittest import TestCase
+from stackcrawler.HackerNewsParser import HackerNewsParser
+from stackcrawler.HackerNewsEntry import HackerNewsEntry
+
+
+
+class TestHackerNewsParser(TestCase):
+  
+
+  def test_parse_entries_from_HTML(self):
+    html_content = open('./tests/news_ycombinator_sample.html', 'r').read()
+    erxpected = [
+      HackerNewsEntry(
+       order_number=1,
+       title='Scrollbars Are Becoming a Problem',
+       comments_count=67,
+       points=110 
+      ),
+
+      HackerNewsEntry(
+        order_number=2,
+        title="First word discovered in unopened Herculaneum scroll by CS student",
+        comments_count=108,
+        points=458
+      )
+    ]
+
+    entries = HackerNewsParser.parse_entries_from_HTML(html_content)
+    self.assertEqual(erxpected, entries)
