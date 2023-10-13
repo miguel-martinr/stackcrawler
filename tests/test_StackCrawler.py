5ae8528992c0bb1262fb8fc616a943b3b84f9e82
Miguel Alejandro Martín <alu0101209777@ull.edu.es>
2023-10-13 13:59:10 +0100

    fetch_entries_from_HTML with limmit failing

--- tests/test_StackCrawler.py
+++ tests/test_StackCrawler.py
@@ -12,3 +12,8 @@ class TestStackCrawler(TestCase):
     self.assertEqual(30, len(entries))
     self.assertEqual(1, entries[0].order_number)
     self.assertEqual(30, entries[29].order_number)
+
+    entries = stack_crawler.fetch_entries_from_HTML(url='https://news.ycombinator.com/', limit=25)
+    self.assertEqual(25, len(entries))
+    self.assertEqual(1, entries[0].order_number)
+    self.assertEqual(15, entries[24].order_number)
